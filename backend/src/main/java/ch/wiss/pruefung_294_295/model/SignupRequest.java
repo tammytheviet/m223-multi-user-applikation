203cc0717f987a4f97372451f52438dd95e4a1b2
nguyenlai0912 <nguyenlai0912@gmail.com>
2023-03-22 11:30:30 +0100

    Update SignupRequest.java

--- backend/src/main/java/ch/wiss/pruefung_294_295/model/SignupRequest.java
+++ backend/src/main/java/ch/wiss/pruefung_294_295/model/SignupRequest.java
@@ -15,7 +15,7 @@ public class SignupRequest {
     @Email
     private String email;
 
-    private Set < String > role;
+    private Set<String> role;
 
     @NotBlank
     @Size(min = 6, max = 40)
@@ -45,11 +45,11 @@ public class SignupRequest {
         this.password = password;
     }
 
-    public Set < String > getRole() {
+    public Set<String> getRole() {
         return this.role;
     }
 
-    public void setRole(Set < String > role) {
+    public void setRole(Set<String> role) {
         this.role = role;
     }
 }
