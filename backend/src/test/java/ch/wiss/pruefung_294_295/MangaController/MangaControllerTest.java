52a65ef58e055e4e9eaae0192ddb7b31bdec0983
tammy <nguyenlai0912@gmail.com>
2023-03-24 19:51:12 +0100

    removed unnecessary imports

--- backend/src/test/java/ch/wiss/pruefung_294_295/MangaController/MangaControllerTest.java
+++ backend/src/test/java/ch/wiss/pruefung_294_295/MangaController/MangaControllerTest.java
@@ -4,7 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import ch.wiss.pruefung_294_295.controller.ChapterController;
 import ch.wiss.pruefung_294_295.controller.MangaController;
