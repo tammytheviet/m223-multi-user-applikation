ecf5c778b537a62c10ad606791e41e8c49f2281d
Fabo <fabiogabriel@hotmail.ch>
2023-03-24 21:07:03 +0100

    Moved Layouts and changed App.js
    Moved Layouts to Layouts.js and changed BrowserRouter to GlobalNavigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,20 @@
 import "./App.css"
 import ReactDOM from "react-dom";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Manga from "./components/Manga";
 import MangaList from "./components/MangaList";
 import MangaUpdate from "./components/MangaUpdate";
 import Layout from "./components/Layout";
 import Chapter from "./components/Chapter";
+import GlobalNavigation from "./GlobalNavigation";
 
 const App = () => {
   return (
     <div className="App">
-    <BrowserRouter>
+    <GlobalNavigation/>
       <Routes>
         <Route path="/" element={<Layout />}>
+        <Route index element={<MangaList />} />
         <Route path="login" element={<Login />} />
         <Route path="register" element={<Register />} />
         <Route path="manga" element={<Manga />} />
@@ -21,7 +23,6 @@ const App = () => {
         <Route path="chapter" element={<Chapter />} />
         </Route>
       </Routes>
-    </BrowserRouter>
     </div>
   );
 }
@@ -36,6 +37,7 @@ const Layout = () => {
           <Outlet/>
         </header>
       </div>
-    </div>);}
+    </div>);
+  }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+export default App;
