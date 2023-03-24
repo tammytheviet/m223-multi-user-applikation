3bb0ec4fb13593d86efb5f1d9b93598844960634
tammy <nguyenlai0912@gmail.com>
2023-03-24 20:17:11 +0100

    SQL Skript im Application.java

--- backend/src/main/java/ch/wiss/pruefung_294_295/Application.java
+++ backend/src/main/java/ch/wiss/pruefung_294_295/Application.java
@@ -1,5 +1,10 @@
 package ch.wiss.pruefung_294_295;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,8 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+        SpringApplication.run(Application.class, args);
+
+        // JDBC code to execute SQL script
+        try (Connection conn = DriverManager.getConnection("jdbc:mariadb://localhost:3306/mangalib", "root", "root");
+                Statement stmt = conn.createStatement();) {
+            String sql1 = "INSERT INTO role (name) VALUES ('ROLE_USER');";
+			String sql2 = "INSERT INTO role (name) VALUES ('ROLE_ADMIN');";
+            stmt.executeUpdate(sql1);
+			stmt.executeUpdate(sql2);
+            System.out.println("SQL script executed successfully.");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
 	@GetMapping("/")
 	public String index() {
