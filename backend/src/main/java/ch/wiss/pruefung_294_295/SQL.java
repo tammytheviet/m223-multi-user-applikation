8e43f0ea64a89a300d8dc82d09119af392b620d2
nguyenlai0912 <nguyenlai0912@gmail.com>
2023-04-04 11:55:45 +0200

    Javadoc - Dokumentieren vom Code

--- backend/src/main/java/ch/wiss/pruefung_294_295/SQL.java
+++ backend/src/main/java/ch/wiss/pruefung_294_295/SQL.java
@@ -4,20 +4,50 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.Statement;
 
-public class SQL {
+/**
+ * Diese Klasse wird verwendet, um die Datenbank 'mangalib' und die Tabelle 'role' in der Datenbank 'mangalib' zu erstellen.
+ * Es werden auch die Rollen 'ROLE_USER' und 'ROLE_ADMIN' in die Tabelle 'role' eingefügt.
+ * @author Tam Lai Nguyen, Fabio Facundo
+ * @version 1.0
+ * @param username: Benutzername für die Datenbank 'mangalib'
+ * @param password: Passwort für die Datenbank 'mangalib'
+ * @param script1: SQL-Script, dass die Rolle 'ROLE_USER' in die Tabelle 'role' in der Datenbank 'mangalib' einfügt
+ * @param script2: SQL-Script, dass die Rolle 'ROLE_ADMIN' in die Tabelle 'role' in der Datenbank 'mangalib' einfügt
+ */
 
-    final static public String username = "root";
-    final static public String password = "root";
+public class SQL 
+{
+
+    //unveränderlicher Benutzername
+    final static private String username = "root";
+
+    //unveränderliches Passwort
+    final static private String password = "root";
     
-    public static void SQLscript() {
+    public static void SQLscript()
+    {
+        //Erstellt eine Verbindung zur Datenbank 'mangalib' mit dem Benutzernamen 'root' und dem Passwort 'root'
         try (Connection c = DriverManager.getConnection("jdbc:mariadb://localhost:3306/mangalib", username, password);
-                Statement s = c.createStatement();) {
-            String script1 = "INSERT INTO role (name) VALUES ('ROLE_USER');"; //Inserts the role 'ROLE_USER' into the table 'role' in the database 'mangalib'
-			String script2 = "INSERT INTO role (name) VALUES ('ROLE_ADMIN');"; //Inserts the role 'ROLE_ADMIN' into the table 'role' in the database 'mangalib'
-            s.executeUpdate(script1); //executes the first script
-			s.executeUpdate(script2); //executes the second script
-        } catch (Exception ex) {
-            ex.printStackTrace(); //handles exceptions and errors
+
+            //Erstellt ein Statement, dass SQL-Scripte ausführen kann
+             Statement s = c.createStatement();)
+             
+        {
+            //Fügt die Rolle 'ROLE_USER' in die Tabelle 'role' in der Datenbank 'mangalib' ein
+            String script1 = "INSERT INTO role (name) VALUES ('ROLE_USER');";
+
+            //Fügt die Rolle 'ROLE_ADMIN' in die Tabelle 'role' in der Datenbank 'mangalib' ein
+			String script2 = "INSERT INTO role (name) VALUES ('ROLE_ADMIN');";
+
+            //Führt die beiden SQL-Scripte aus
+            s.executeUpdate(script1);
+			s.executeUpdate(script2);
+        }
+
+        //Falls ein Fehler auftritt, wird die Exception abgefangen und der Stacktrace des Fehlers wird ausgegeben
+        catch (Exception ex)
+        {
+            ex.printStackTrace();
         }
     }
 }
