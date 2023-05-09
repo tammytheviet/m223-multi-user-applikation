cd32f908ae9f297216a9af276a19d88b49707ccb
tammy <nguyenlai0912@gmail.com>
2023-05-09 23:43:17 +0200

    Front-End Tests
    Still not working because of axios

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -5,4 +5,29 @@ test('renders learn react link', () => {
   render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
+
+  // Additional checks
+  const appElement = screen.getByTestId('app');
+  expect(appElement).toBeInTheDocument();
+
+  const headerElement = screen.getByTestId('header');
+  expect(headerElement).toBeInTheDocument();
 });
+
+  test('renders without crashing', () => {
+    render(<App />);
+    const appElement = screen.getByTestId('app');
+    expect(appElement).toBeInTheDocument();
+  });
+
+  test('displays the correct title', () => {
+    render(<App />);
+    const titleElement = screen.getByText(/Welcome to my App/i);
+    expect(titleElement).toBeInTheDocument();
+  });
+
+  test('displays the correct message', () => {
+    render(<App />);
+    const messageElement = screen.getByText(/This is a sample message/i);
+    expect(messageElement).toBeInTheDocument();
+  });
